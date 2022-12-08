4bf83bc8092d7575be8516ca518a71ce8397f5ee
Oğuz Kürşat ÖZKAN <114420537+oguzkursat@users.noreply.github.com>
2022-12-08 21:43:31 +0300

    Update Class1.java
    New changes

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -5,13 +5,13 @@ public class Class1 {
 
         System.out.println("Hello world");
 
-        //System.out.println("Hello world");
-
         //world1
 
         //world2
 
         //world3
 
+        //System.out.println("Hello world");
+        
     }
 }
